ccdc4d1bcdbddb403f8918ed8bede53afb08bfec
Dhaval Vadgama <dhaval.it@gmail.com>
2022-12-30 18:31:06 +0000

    cleanup: resources

--- svc/django/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class SampleTable(models.Model):
-    sampleid = models.CharField(max_length=90)
-    sample_name = models.CharField(max_length=60)
